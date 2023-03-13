eaf501a51e0c685e7cefff087230575140d22c0d
User <User@WINDEV2209EVAL>
2023-03-13 06:41:57 -0700

    Add project files.

---
+++ Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Models
+{
+    public class PersonModel
+    {
+        public int User_ID { get; set; }
+        public string User_Email { get; set; }
+    }
+}
