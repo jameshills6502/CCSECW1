e093e9eb3bdc3ab78ede4b78baf23ea3f8c7d634
jameshills6502 <u2141560@live.warwick.ac.uk>
2023-03-13 10:54:29 -0700

    changes made to personmodel and component 3

--- Models/PersonModel.cs
+++ Models/PersonModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace BlazorServer.Models
@@ -17,5 +18,27 @@ namespace BlazorServer.Models
         public string User_Email { get; set; }
         public string User_Password { get; set; }
 
+    }
+    public class CarModel
+    {
+        public int Car_ID { get; set; }
+        public Blob Car_Image { get; set; }
+        public string Car_Make { get; set; }
+        public string Car_Series { get; set; }
+
+        public string Car_Type { get; set; }
+
+        public float Car_Cost { get; set; }
+    }
+
+    public class FinanceModel
+    {
+        public int Request_ID { get; set; }
+        public string Approval_Status { get; set; }
+        public float Amount_To_Finance { get; set; }
+        public int Admin_Accounts_Admin_ID { get; set; }
+
+        public int Car_Car_ID { get; set; }
+        public int User_Accounts_User_ID { get; set; }
     }
 }
