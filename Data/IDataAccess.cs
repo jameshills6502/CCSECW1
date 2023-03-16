4e002eef8e9b3a6972bf7e9598af5dc50400d47a
jameshills6502 <u2141560@live.warwick.ac.uk>
2023-03-16 03:33:03 +0000

    Changes made to UserModels and Data Access

--- Data/IDataAccess.cs
+++ Data/IDataAccess.cs
@@ -2,7 +2,7 @@
 {
     public interface IDataAccess
     {
-        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
+        Task<List<T>?> LoadData<T, U>(string sql, U parameters, string connectionString);
         Task SaveData<T>(string sql, T parameters, string connectionString);
     }
 }
---
+++ Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServer.Models
+{
+    public class User
+    {
+        [Required][EmailAddress]public string Email { get; set;}
+        [Required][MinLength(6, ErrorMessage= "Password must be 6 characters long")]public string Password { get; set;}
+    }
+}
